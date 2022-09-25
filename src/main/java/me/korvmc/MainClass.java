85b6515f7b97d5cb74a6c900823b6dd2167af6cb
Lendi24 <Lendic24@hotmail.com>
2022-09-25 12:52:03 +0200

    Update MainClass.java

--- src/main/java/me/korvmc/MainClass.java
+++ src/main/java/me/korvmc/MainClass.java
@@ -1,8 +1,23 @@
 package me.korvmc;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.network.MessageType;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Vec3d;
+
+import net.minecraft.client.gui.ClientChatListener;
+
+import java.util.UUID;
 
 public class MainClass implements ModInitializer {
     // This logger is used to write text to the console and the log file.
@@ -17,5 +32,39 @@ public class MainClass implements ModInitializer {
         // Proceed with mild caution.
 
         LOGGER.info("Hello Fabric world!");
+
+        ClientTickEvents.END_CLIENT_TICK.register((evt) -> {
+            LOGGER.info(String.valueOf(evt.player.getPos().x));
+
+            //            long x = ((long)player.poss)
+        });
+
+        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) ->
+        {
+            BlockState state = world.getBlockState(pos);
+            /* Manual spectator check is necessary because AttackBlockCallbacks
+               fire before the spectator check */
+            if (state.isToolRequired() && !player.isSpectator() &&
+                    player.getMainHandStack().isEmpty())
+            {
+                player.damage(DamageSource.GENERIC, 1.0F);
+            }
+            player.setVelocityClient(1,0,0);
+            LOGGER.info("Hit smt event!");
+
+
+            return ActionResult.PASS;
+        });
     }
+
+    public void tick(MinecraftClient client) {
+        if (client.player != null) {
+            client.player.setVelocityClient(10,0,0);
+        }
+    }
+
+    public void onChatMessage(MessageType type, Text message, UUID sender) {
+        LOGGER.info("Chat: "+ message);
+    }
+
 }
